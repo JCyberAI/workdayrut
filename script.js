7186d1d4fb8bdec213bc5fb739bc610ee7222d84
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-10 17:52:08 -0500

    commit on main

---
+++ script.js
@@ -0,0 +1,14 @@
+var timer = setInterval(checkTime, 1000)
+
+function checkTime(){
+    let dateTime = new Date()
+    $("#currentDay").html(dateTime)
+    hourNow = dateTime.getHours()
+    let offSet = 0
+    if (hourNow > 17){offSet = 8}
+    else if (hourNow < 9) {offSet = -8}
+    hourNow = hourNow - offSet 
+    if (oldHour < hourNow){
+        formatField(hourNow)
+    }
+}
